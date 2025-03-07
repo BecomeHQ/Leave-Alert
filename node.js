295d7653ee39e41685e0ba192fc3dfa3d81157a5
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2025-03-07 19:49:13 +0530

    changes port

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 23000;
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cron = require("node-cron");
