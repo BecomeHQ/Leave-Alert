0abebb1ef428aa32d7af067e2e60386ce4aaf334
Noothan-am <noothanam13@gmail.com>
2026-02-07 10:14:33 +0530

    removes notification for weekends

--- node.js
+++ node.js
@@ -156,7 +156,7 @@ async function sendSlackNotification(leaves) {
   }
 }
 
-cron.schedule("0 9 * * *", async () => {
+cron.schedule("0 9 * * 1-5", async () => {
   console.log("Running scheduled task to send Slack notifications...");
   const today = new Date();
   const startOfToday = new Date(today);
